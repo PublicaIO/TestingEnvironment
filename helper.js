3347a4129b8888acfc7447849cc18964e0489ad0
arturslataks <arturs@majaslapa.lv>
2018-02-06 11:15:09 +0200

    Updated helper.js file with contract deploy and instance get methods

--- helper.js
+++ helper.js
@@ -42,3 +42,35 @@ function sendEther(amount, receiver) {
         value: web3.toWei(amount, 'ether')
     });
 }
+
+function getContract(abi, address) {
+    var instance = eth.contract(abi).at(address);
+
+    return instance;
+}
+
+function deployContract(abi, bin, params, deployer) {
+    deployer = defaultValue(deployer, eth.coinbase);
+    params = defaultValue(params, []);
+
+    personal.unlockAccount(deployer);
+
+    if (!abi || !bin || !deployer) {
+        console.log('Required data is missing');
+        return false;
+    }
+
+    params.push({ from: deployer, data: bin, gas: '0x3d0900' });
+    params.push(function (error, contract) {
+        if (error) {
+            console.log(error);
+        } else if (contract.address) {
+            console.log('Contract deployed at: ' + contract.address);
+        }
+    })
+
+    contract = eth.contract(abi);
+    contract.new.apply(contract, params);
+
+    mine();
+}
